d14c4bae008d5c2439a234d3d7b20b6de1dc9be2
Shokkoh <ezshokkoh@gmail.com>
2025-10-29 23:59:00 -0300

    Main

---
+++ src/main/java/com/dragonminez/DragonMineZ.java
@@ -0,0 +1,9 @@
+package com.dragonminez;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Reference.MOD_ID)
+public class DragonMineZ {
+	public DragonMineZ() {
+	}
+}
---
+++ src/main/java/com/dragonminez/Reference.java
@@ -0,0 +1,11 @@
+package com.dragonminez;
+
+public class Reference {
+	public static final String MOD_ID = "dragonminez";
+	public static final String VERSION = "2.0.0";
+	public static final String EMPTY = "%s:empty".formatted(MOD_ID);
+
+	// Private constructor to prevent instantiation
+	private Reference() {
+	}
+}
