124716d4e01d28a2d5257f35f4fccfebfe7c1071
Shokkoh <ezshokkoh@gmail.com>
2025-12-15 02:51:18 -0300

    feat: RangedAttribute Mixin

---
+++ src/main/java/com/dragonminez/common/stats/GenericAttributes.java
@@ -0,0 +1,37 @@
+package com.dragonminez.common.stats;
+
+import com.dragonminez.Reference;
+import com.dragonminez.mixin.common.RangedAttributeMixin;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.RangedAttribute;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod.EventBusSubscriber(modid = Reference.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class GenericAttributes {
+
+	@SubscribeEvent
+	public static void onLoadComplete(FMLLoadCompleteEvent event) {
+		Attribute armorAttribute = ForgeRegistries.ATTRIBUTES.getValue(new ResourceLocation("minecraft:generic.armor"));
+		Attribute armorToughnessAttribute = ForgeRegistries.ATTRIBUTES.getValue(new ResourceLocation("minecraft:generic.armor_toughness"));
+		Attribute maxHealth = ForgeRegistries.ATTRIBUTES.getValue(new ResourceLocation("minecraft:generic.max_health"));
+
+		if (armorAttribute instanceof RangedAttribute) {
+			RangedAttributeMixin accessor = (RangedAttributeMixin) armorAttribute;
+			accessor.setMaxValue(1000000.0);
+		}
+
+		if (armorToughnessAttribute instanceof RangedAttribute) {
+			RangedAttributeMixin accessor = (RangedAttributeMixin) armorToughnessAttribute;
+			accessor.setMaxValue(1000000.0);
+		}
+
+		if (maxHealth instanceof RangedAttribute) {
+			RangedAttributeMixin accessor = (RangedAttributeMixin) maxHealth;
+			accessor.setMaxValue(2000000000.0);
+		}
+	}
+}
