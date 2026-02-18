cd11ff1a82066f31fb4e09d1e5aaef758fbf7dd8
YaibaToKen <kurokyoudai@live.com>
2026-02-18 12:41:35 +0000

    Resized the Utility Menu to be a 3x5 grid.
    Refactored the grid initialization to be self contained and avoid duplicate entries.
    
    Made the grid rendering dynamic by not depending on fixed sizes for the grid. We can now define whatever sized grid we want and use that.

--- src/main/java/com/dragonminez/client/gui/UtilityMenuScreen.java
+++ src/main/java/com/dragonminez/client/gui/UtilityMenuScreen.java
@@ -4,18 +4,11 @@ import com.dragonminez.client.gui.utilitymenu.ButtonInfo;
 import com.dragonminez.client.gui.utilitymenu.IUtilityMenuSlot;
 import com.dragonminez.client.gui.utilitymenu.menuslots.*;
 import com.dragonminez.client.util.KeyBinds;
-import com.dragonminez.common.init.MainSounds;
-import com.dragonminez.common.network.C2S.ExecuteActionC2S;
-import com.dragonminez.common.network.C2S.SwitchActionC2S;
-import com.dragonminez.common.network.NetworkHandler;
-import com.dragonminez.common.stats.ActionMode;
 import com.dragonminez.common.stats.StatsCapability;
 import com.dragonminez.common.stats.StatsData;
 import com.dragonminez.common.stats.StatsProvider;
-import com.dragonminez.common.util.TransformationsHelper;
 import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
@@ -28,15 +21,23 @@ import java.util.List;
 
 @OnlyIn(Dist.CLIENT)
 public class UtilityMenuScreen extends Screen {
-	private List<IUtilityMenuSlot> menuSlots = new ArrayList<>();
+	private static final List<IUtilityMenuSlot> MENU_SLOTS = new ArrayList<>();
+	private static final List<IUtilityMenuSlot> ADDON_SLOTS = new ArrayList<>();
+	private static final int[][] POSITIONS = {
+			{-2, -1}, {-1, -1}, {0, -1}, {1, -1}, {2, -1},
+			{-2,  0}, {-1,  0},          {1,  0}, {2,  0},
+			{-2,  1}, {-1,  1}, {0,  1}, {1,  1}, {2,  1}
+	};
+	private static final long ANIMATION_DURATION = 100;
+	// Default button width is 105, but proves too large for GUI Scale AUTO
+	// and GUI Scale 4 when using 14 slot positions
+	private static final int BUTTON_WIDTH = 90;
+	private static final int BUTTON_HEIGHT = 70;
+	private static final int GAP = 5;
 
 	private long openTime;
-	private static final long ANIMATION_DURATION = 100;
 	private StatsData statsData;
 
-	private final int BUTTON_WIDTH = 105;
-	private final int BUTTON_HEIGHT = 70;
-	private final int GAP = 5;
 
 	public UtilityMenuScreen() {
 		super(Component.literal("Menu"));
@@ -51,15 +52,8 @@ public class UtilityMenuScreen extends Screen {
 			StatsProvider.get(StatsCapability.INSTANCE, mc.player).ifPresent(data -> this.statsData = data);
 		}
 
-		IUtilityMenuSlot emptySlot = new EmptyMenuSlot();
-		menuSlots.add(0, new KaiokenMenuSlot());
-		menuSlots.add(1, new SuperformMenuSlot());
-		menuSlots.add(2, new FusionMenuSlot());
-		menuSlots.add(3, emptySlot);
-		menuSlots.add(4, emptySlot);
-		menuSlots.add(5, new KiManipulationMenuSlot());
-		menuSlots.add(6, new RacialActionMenuSlot());
-		menuSlots.add(7, new DescendFormMenuSlot());
+		// FIXME: This should be moved to a mod loading event, perhaps post init
+		initMenuSlots();
 	}
 
 	@Override
@@ -90,15 +84,9 @@ public class UtilityMenuScreen extends Screen {
 	}
 
 	private void renderGrid(GuiGraphics graphics, int centerX, int centerY, int mouseX, int mouseY) {
-		int[][] positions = {
-				{-1, -1}, {0, -1}, {1, -1},
-				{-1, 0},           {1, 0},
-				{-1, 1},  {0, 1},  {1, 1}
-		};
-
-		for (int i = 0; i < 8; i++) {
-			int col = positions[i][0];
-			int row = positions[i][1];
+		for (int i = 0; i < POSITIONS.length; i++) {
+			int col = POSITIONS[i][0];
+			int row = POSITIONS[i][1];
 
 			int x = centerX + (col * (BUTTON_WIDTH + GAP)) - (BUTTON_WIDTH / 2);
 			int y = centerY + (row * (BUTTON_HEIGHT + GAP)) - (BUTTON_HEIGHT / 2);
@@ -113,7 +101,7 @@ public class UtilityMenuScreen extends Screen {
 	}
 
 	private void renderButtonContent(GuiGraphics graphics, int index, int x, int y) {
-		IUtilityMenuSlot menuSlot = menuSlots.get(index);
+		IUtilityMenuSlot menuSlot = MENU_SLOTS.get(index);
 		if (menuSlot != null) {
 			ButtonInfo buttonInfo = menuSlot.render(statsData);
 			if (buttonInfo != null) {
@@ -122,9 +110,13 @@ public class UtilityMenuScreen extends Screen {
 				}
 
 				if (!buttonInfo.getLine1().getString().isEmpty()) {
-					drawCenteredStringWithBorder(graphics, buttonInfo.getLine1(), x + BUTTON_WIDTH / 2, y + 12, 0xFFFFFF, 0x000000);
-					if (index == 5) graphics.drawCenteredString(font, buttonInfo.getLine2(), x + BUTTON_WIDTH / 2, y + 30, statsData.getSkills().isSkillActive("kimanipulation") ? 0x2BFF00 : 0xFF1B00);
-					else graphics.drawCenteredString(font, buttonInfo.getLine2(), x + BUTTON_WIDTH / 2, y + 30, buttonInfo.isSelected() ? buttonInfo.getColor() : 0xFF1B00);
+					this.drawCenteredStringWithBorder(graphics, buttonInfo.getLine1(), x + BUTTON_WIDTH / 2, y + 12, 0xFFFFFF, 0x000000);
+					if (index == 5) {
+						graphics.drawCenteredString(font, buttonInfo.getLine2(), x + BUTTON_WIDTH / 2, y + 30, statsData.getSkills().isSkillActive("kimanipulation") ? 0x2BFF00 : 0xFF1B00);
+					}
+					else {
+						graphics.drawCenteredString(font, buttonInfo.getLine2(), x + BUTTON_WIDTH / 2, y + 30, buttonInfo.isSelected() ? buttonInfo.getColor() : 0xFF1B00);
+					}
 				}
 			}
 		}
@@ -135,15 +127,9 @@ public class UtilityMenuScreen extends Screen {
 		int centerX = this.width / 2;
 		int centerY = this.height / 2;
 
-		int[][] positions = {
-				{-1, -1}, {0, -1}, {1, -1},
-				{-1, 0},           {1, 0},
-				{-1, 1},  {0, 1},  {1, 1}
-		};
-
-		for (int i = 0; i < 8; i++) {
-			int col = positions[i][0];
-			int row = positions[i][1];
+		for (int i = 0; i < POSITIONS.length; i++) {
+			int col = POSITIONS[i][0];
+			int row = POSITIONS[i][1];
 
 			int x = centerX + (col * (BUTTON_WIDTH + GAP)) - (BUTTON_WIDTH / 2);
 			int y = centerY + (row * (BUTTON_HEIGHT + GAP)) - (BUTTON_HEIGHT / 2);
@@ -157,7 +143,7 @@ public class UtilityMenuScreen extends Screen {
 	}
 
 	private void handleSlotClick(int index) {
-		IUtilityMenuSlot menuSlot = menuSlots.get(index);
+		IUtilityMenuSlot menuSlot = MENU_SLOTS.get(index);
 		if (menuSlot != null) {
 			menuSlot.handle(statsData);
 		}
@@ -184,5 +170,30 @@ public class UtilityMenuScreen extends Screen {
 		graphics.drawString(font, text, x - font.width(text) / 2, y - 1, borderColor, false);
 		graphics.drawString(font, text, x - font.width(text) / 2, y + 1, borderColor, false);
 		graphics.drawString(font, text, x - font.width(text) / 2, y, color, false);
+	}
+
+	public static void initMenuSlots() {
+		if (MENU_SLOTS.isEmpty()) {
+			MENU_SLOTS.add(0, new KaiokenMenuSlot());
+			MENU_SLOTS.add(1, new SuperformMenuSlot());
+			MENU_SLOTS.add(2, new FusionMenuSlot());
+			MENU_SLOTS.add(3, new EmptyMenuSlot());
+			MENU_SLOTS.add(4, new EmptyMenuSlot());
+			MENU_SLOTS.add(5, new KiManipulationMenuSlot());
+			MENU_SLOTS.add(6, new RacialActionMenuSlot());
+			MENU_SLOTS.add(7, new DescendFormMenuSlot());
+			if (!ADDON_SLOTS.isEmpty()) {
+				for (int i = MENU_SLOTS.size(), e = 0; i < POSITIONS.length && e <= ADDON_SLOTS.size(); i++, e++) {
+					MENU_SLOTS.add(i, ADDON_SLOTS.get(e));
+				}
+			}
+			for (int i = MENU_SLOTS.size(); i < POSITIONS.length; i++) {
+				MENU_SLOTS.add(i, new EmptyMenuSlot());
+			}
+		}
+	}
+
+	public static void addMenuSlot(IUtilityMenuSlot menuSlot) {
+		ADDON_SLOTS.add(menuSlot);
 	}
 }
