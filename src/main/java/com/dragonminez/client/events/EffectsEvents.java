93bb4841389662c3ea7654506e09b9a60b1a9205
Yuse <dorugamex@gmail.com>
2026-02-19 00:49:22 -0500

    Transformations Shake Camara

--- src/main/java/com/dragonminez/client/events/EffectsEvents.java
+++ src/main/java/com/dragonminez/client/events/EffectsEvents.java
@@ -2,11 +2,15 @@ package com.dragonminez.client.events;
 
 import com.dragonminez.Reference;
 import com.dragonminez.common.init.MainEffects;
+import com.dragonminez.common.init.MainParticles;
+import com.dragonminez.common.stats.ActionMode;
 import com.dragonminez.common.stats.StatsCapability;
 import com.dragonminez.common.stats.StatsProvider;
+import com.dragonminez.common.util.ComboManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
@@ -31,20 +35,46 @@ public class EffectsEvents {
         Minecraft mc = Minecraft.getInstance();
         Player player = mc.player;
 
-        if (player == null || mc.isPaused() || !player.hasEffect(MainEffects.STAGGER.get())) return;
-
-        int amplifier = player.getEffect(MainEffects.STAGGER.get()).getAmplifier();
-        float baseIntensity = 1.5F + (amplifier * 0.8F);
+        if (player == null || mc.isPaused()) return;
 
         double time = player.level().getGameTime() + event.getPartialTick();
 
-        float yawShake = (float) (Math.sin(time * 0.5D) * baseIntensity * 0.8F + Math.sin(time * 1.2D) * baseIntensity * 0.4F);
-        float pitchShake = (float) (Math.cos(time * 0.6D) * baseIntensity * 2.0F + Math.cos(time * 0.9D) * baseIntensity * 1.0F);
-        float rollShake = (float) (Math.sin(time * 0.7D) * baseIntensity * 0.6F);
+        if (player.hasEffect(MainEffects.STAGGER.get())) {
+            int amplifier = player.getEffect(MainEffects.STAGGER.get()).getAmplifier();
+            float baseIntensity = 1.5F + (amplifier * 0.8F);
 
-        event.setYaw(event.getYaw() + yawShake);
-        event.setPitch(event.getPitch() + pitchShake);
-        event.setRoll(event.getRoll() + rollShake);
+            float yawShake = (float) (Math.sin(time * 0.5D) * baseIntensity * 0.8F + Math.sin(time * 1.2D) * baseIntensity * 0.4F);
+            float pitchShake = (float) (Math.cos(time * 0.6D) * baseIntensity * 2.0F + Math.cos(time * 0.9D) * baseIntensity * 1.0F);
+            float rollShake = (float) (Math.sin(time * 0.7D) * baseIntensity * 0.6F);
+
+            event.setYaw(event.getYaw() + yawShake);
+            event.setPitch(event.getPitch() + pitchShake);
+            event.setRoll(event.getRoll() + rollShake);
+        }
+
+        StatsProvider.get(StatsCapability.INSTANCE, player).ifPresent(data -> {
+            if (data.getStatus().isActionCharging() && data.getStatus().getSelectedAction() == ActionMode.FORM) {
+
+                double time2 = player.level().getGameTime() + event.getPartialTick();
+
+                float globalshake = 0.05F;
+
+                float sweepIntensity = 2.5F * globalshake;
+                float sweepYaw = (float) (Math.sin(time2 * 1.5D) * sweepIntensity);
+                float sweepPitch = (float) (Math.cos(time2 * 1.2D) * (sweepIntensity * 0.6F));
+
+                float shakeIntensity = 0.2F * globalshake;
+                float shakeYaw = (float) (Math.sin(time2 * 5.0D) * shakeIntensity);
+                float shakePitch = (float) (Math.cos(time2 * 6.0D) * shakeIntensity);
+
+                float rollIntensity = 1.5F * globalshake;
+                float tfRoll = (float) (Math.sin(time2 * 2.0D) * rollIntensity);
+
+                event.setYaw(event.getYaw() + sweepYaw + shakeYaw);
+                event.setPitch(event.getPitch() + sweepPitch + shakePitch);
+                event.setRoll(event.getRoll() + tfRoll);
+            }
+        });
     }
 
 	@SubscribeEvent
