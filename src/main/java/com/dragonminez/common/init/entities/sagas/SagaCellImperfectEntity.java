528148d05227a996793df878ea1158952304392e
Shokkoh <ezshokkoh@gmail.com>
2026-02-27 11:47:08 -0300

    fix: nerf absorption skill (npcs)

--- src/main/java/com/dragonminez/common/init/entities/sagas/SagaCellImperfectEntity.java
+++ src/main/java/com/dragonminez/common/init/entities/sagas/SagaCellImperfectEntity.java
@@ -2,8 +2,6 @@ package com.dragonminez.common.init.entities.sagas;
 
 import com.dragonminez.common.init.entities.IBattlePower;
 import com.dragonminez.common.stats.StatsCapability;
-import com.dragonminez.common.stats.StatsProvider;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -20,166 +18,161 @@ import software.bernie.geckolib.core.object.PlayState;
 
 public class SagaCellImperfectEntity extends DBSagasEntity {
 
-    private static final int SKILL_KAMEHA = 1;
-    private static final int SKILL_ABSORBER = 2;
+	private static final int SKILL_KAMEHA = 1;
+	private static final int SKILL_ABSORBER = 2;
 
-    private int kamehaCooldown = 0;
-    private int absorberCooldown = 0;
+	private int kamehaCooldown = 0;
+	private int absorberCooldown = 0;
 
-    public SagaCellImperfectEntity(EntityType<? extends Monster> pEntityType, Level pLevel) {
-        super(pEntityType, pLevel);
-        this.evade(true, 40);
-        if (this instanceof IBattlePower bp) {
-            bp.setBattlePower(750000000);
-        }
-    }
+	public SagaCellImperfectEntity(EntityType<? extends Monster> pEntityType, Level pLevel) {
+		super(pEntityType, pLevel);
+		this.evade(true, 40);
+		if (this instanceof IBattlePower bp) {
+			bp.setBattlePower(750000000);
+		}
+	}
 
-    @Override
-    public void tick() {
-        super.tick();
+	@Override
+	public void tick() {
+		super.tick();
 
-        LivingEntity target = this.getTarget();
+		LivingEntity target = this.getTarget();
 
-        boolean isAbsorbing = this.isCasting() && this.getSkillType() == SKILL_ABSORBER;
-        handleCommonCombatMovement(target, this.isCasting() && !isAbsorbing, true);
+		boolean isAbsorbing = this.isCasting() && this.getSkillType() == SKILL_ABSORBER;
+		handleCommonCombatMovement(target, this.isCasting() && !isAbsorbing, true);
 
-        if (!this.level().isClientSide) {
-            if (this.kamehaCooldown > 0) this.kamehaCooldown--;
-            if (this.absorberCooldown > 0) this.absorberCooldown--;
+		if (!this.level().isClientSide) {
+			if (this.kamehaCooldown > 0) this.kamehaCooldown--;
+			if (this.absorberCooldown > 0) this.absorberCooldown--;
 
-            if (target != null && target.isAlive() && !this.isCasting()) {
-                double distSqr = this.distanceToSqr(target);
+			if (target != null && target.isAlive() && !this.isCasting()) {
+				double distSqr = this.distanceToSqr(target);
 
-                // Iniciar Absorción
-                if (this.absorberCooldown <= 0 && distSqr < 9.0D) {
-                    startCasting(SKILL_ABSORBER);
-                }
-                // Iniciar Kamehameha
-                else if (this.kamehaCooldown <= 0 && distSqr > 100.0D) {
-                    startCasting(SKILL_KAMEHA);
-                }
-                else if (this.teleportCooldown <= 0 && distSqr > 64.0D) {
-                    performTeleport(target);
-                }
-            }
+				// Iniciar Absorción
+				if (this.absorberCooldown <= 0 && distSqr < 9.0D) {
+					startCasting(SKILL_ABSORBER);
+				}
+				// Iniciar Kamehameha
+				else if (this.kamehaCooldown <= 0 && distSqr > 100.0D) {
+					startCasting(SKILL_KAMEHA);
+				} else if (this.teleportCooldown <= 0 && distSqr > 64.0D) {
+					performTeleport(target);
+				}
+			}
 
-            if (this.isCasting()) {
-                this.setDeltaMovement(this.getDeltaMovement().multiply(0.5, 0.5, 0.5));
+			if (this.isCasting()) {
+				this.setDeltaMovement(this.getDeltaMovement().multiply(0.5, 0.5, 0.5));
 
-                if (target != null && target.isAlive()) {
-                    this.castTimer++;
-                    int currentSkill = getSkillType();
+				if (target != null && target.isAlive()) {
+					this.castTimer++;
+					int currentSkill = getSkillType();
 
-                    if (currentSkill == SKILL_KAMEHA) {
-                        this.lookAt(target, 30.0F, 30.0F);
-                        if (this.castTimer >= 50) {
-                            shootGenericKiWave(target, 2.0F, 0xB0FDFF, 0x40FAFF, 1.5f);
-                            stopCasting();
-                        }
-                    }
+					if (currentSkill == SKILL_KAMEHA) {
+						this.lookAt(target, 30.0F, 30.0F);
+						if (this.castTimer >= 50) {
+							shootGenericKiWave(target, 2.0F, 0xB0FDFF, 0x40FAFF, 1.5f);
+							stopCasting();
+						}
+					} else if (currentSkill == SKILL_ABSORBER) {
 
-                    else if (currentSkill == SKILL_ABSORBER) {
+						performTeleportBehind(target);
 
-                        performTeleportBehind(target);
+						this.lookAt(target, 360.0F, 360.0F);
 
-                        this.lookAt(target, 360.0F, 360.0F);
+						if (this.castTimer % 5 == 0) {
+							drainLifeAndEnergy(target);
+						}
 
-                        if (this.castTimer % 5 == 0) {
-                            drainLifeAndEnergy(target);
-                        }
+						if (this.castTimer >= 120) {
+							stopCasting();
+						}
+					}
 
-                        if (this.castTimer >= 120) {
-                            stopCasting();
-                        }
-                    }
+				} else {
+					stopCasting();
+				}
+			}
+		}
+	}
 
-                } else {
-                    stopCasting();
-                }
-            }
-        }
-    }
+	private void drainLifeAndEnergy(LivingEntity target) {
+		float damageToDeal = getKiBlastDamage() / 2;
+		if (damageToDeal < 1.0F) damageToDeal = 1.0F;
 
-    private void drainLifeAndEnergy(LivingEntity target) {
-        float damageToDeal = getKiBlastDamage() * 0.5F;
-        if (damageToDeal < 1.0F) damageToDeal = 1.0F;
+		target.hurt(this.damageSources().magic(), damageToDeal);
 
-        target.hurt(this.damageSources().magic(), damageToDeal);
+		if (target instanceof Player player) {
+			player.getCapability(StatsCapability.INSTANCE).ifPresent(stats -> {
+				int maxEnergy = stats.getMaxEnergy();
 
-        if (target instanceof Player player) {
-            player.getCapability(StatsCapability.INSTANCE).ifPresent(stats -> {
-                int maxEnergy = stats.getMaxEnergy();
+				int drainPercentage = (int) (maxEnergy * 0.05);
+				if (drainPercentage < 1) drainPercentage = 1;
 
-                int drainPercentage = (int) (maxEnergy * 0.05);
-                if (drainPercentage < 1) drainPercentage = 1;
+				stats.getResources().removeEnergy(drainPercentage);
+			});
+		}
 
-                stats.getResources().removeEnergy(drainPercentage);
-            });
-        }
+		this.heal(damageToDeal);
+	}
 
-        this.heal(damageToDeal);
-    }
+	private void performTeleportBehind(LivingEntity target) {
+		Vec3 look = target.getLookAngle().normalize();
 
-    private void performTeleportBehind(LivingEntity target) {
-        Vec3 look = target.getLookAngle().normalize();
+		double distance = 0.3D;
 
-        double distance = 0.3D;
+		double destX = target.getX() - (look.x * distance);
+		double destZ = target.getZ() - (look.z * distance);
 
-        double destX = target.getX() - (look.x * distance);
-        double destZ = target.getZ() - (look.z * distance);
+		double destY = target.getY();
 
-        double destY = target.getY();
+		this.setPos(destX, destY, destZ);
+	}
 
-        this.setPos(destX, destY, destZ);
-    }
+	@Override
+	public boolean hurt(DamageSource source, float amount) {
+		if (this.isCasting() && this.getSkillType() == SKILL_ABSORBER) {
+			return false;
+		}
+		return super.hurt(source, amount);
+	}
 
-    @Override
-    public boolean hurt(DamageSource source, float amount) {
-        if (this.isCasting() && this.getSkillType() == SKILL_ABSORBER) {
-            return false;
-        }
-        return super.hurt(source, amount);
-    }
+	@Override
+	public void stopCasting() {
+		int usedSkill = getSkillType();
 
-    @Override
-    public void stopCasting() {
-        int usedSkill = getSkillType();
+		if (usedSkill == SKILL_KAMEHA) {
+			this.kamehaCooldown = 10 * 20;
+		} else if (usedSkill == SKILL_ABSORBER) {
+			this.absorberCooldown = 20 * 20;
+		}
 
-        if (usedSkill == SKILL_KAMEHA) {
-            this.kamehaCooldown = 10 * 20;
-        }
-        else if (usedSkill == SKILL_ABSORBER) {
-            this.absorberCooldown = 20 * 20;
-        }
+		super.stopCasting();
+	}
 
-        super.stopCasting();
-    }
+	@Override
+	public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+		super.registerControllers(controllers);
+		controllers.add(new AnimationController<>(this, "skill_controller", 0, this::skillPredicate));
+		controllers.add(new AnimationController<>(this, "tail", 0, this::tailPredicate));
+	}
 
-    @Override
-    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
-        super.registerControllers(controllers);
-        controllers.add(new AnimationController<>(this, "skill_controller", 0, this::skillPredicate));
-        controllers.add(new AnimationController<>(this, "tail", 0, this::tailPredicate));
-    }
+	private <T extends GeoAnimatable> PlayState skillPredicate(AnimationState<T> event) {
+		if (this.isCasting()) {
+			int currentSkill = getSkillType();
 
-    private <T extends GeoAnimatable> PlayState skillPredicate(AnimationState<T> event) {
-        if (this.isCasting()) {
-            int currentSkill = getSkillType();
+			if (currentSkill == SKILL_KAMEHA) {
+				return event.setAndContinue(RawAnimation.begin().thenPlay("kiwave"));
+			} else if (currentSkill == SKILL_ABSORBER) {
+				return event.setAndContinue(RawAnimation.begin().thenLoop("absorb"));
+			}
 
-            if (currentSkill == SKILL_KAMEHA) {
-                return event.setAndContinue(RawAnimation.begin().thenPlay("kiwave"));
-            }
-            else if (currentSkill == SKILL_ABSORBER) {
-                return event.setAndContinue(RawAnimation.begin().thenLoop("absorb"));
-            }
+			return PlayState.CONTINUE;
+		}
+		event.getController().forceAnimationReset();
+		return PlayState.STOP;
+	}
 
-            return PlayState.CONTINUE;
-        }
-        event.getController().forceAnimationReset();
-        return PlayState.STOP;
-    }
-
-    private <T extends GeoAnimatable> PlayState tailPredicate(AnimationState<T> event) {
-        return event.setAndContinue(RawAnimation.begin().thenLoop("tail"));
-    }
+	private <T extends GeoAnimatable> PlayState tailPredicate(AnimationState<T> event) {
+		return event.setAndContinue(RawAnimation.begin().thenLoop("tail"));
+	}
 }
--- src/main/java/com/dragonminez/common/init/entities/sagas/SagaDrGeroEntity.java
+++ src/main/java/com/dragonminez/common/init/entities/sagas/SagaDrGeroEntity.java
@@ -19,158 +19,154 @@ import software.bernie.geckolib.core.object.PlayState;
 
 public class SagaDrGeroEntity extends DBSagasEntity {
 
-    private static final int SKILL_EYE_LASER = 1;
-    private static final int SKILL_ENERGY_DRAIN = 2; // El "Grab"
+	private static final int SKILL_EYE_LASER = 1;
+	private static final int SKILL_ENERGY_DRAIN = 2; // El "Grab"
 
-    private int laserCooldown = 0;
-    private int drainCooldown = 0;
+	private int laserCooldown = 0;
+	private int drainCooldown = 0;
 
-    public SagaDrGeroEntity(EntityType<? extends Monster> pEntityType, Level pLevel) {
-        super(pEntityType, pLevel);
-        if (this instanceof IBattlePower bp) {
-            bp.setBattlePower(150000000);
-        }
-    }
+	public SagaDrGeroEntity(EntityType<? extends Monster> pEntityType, Level pLevel) {
+		super(pEntityType, pLevel);
+		if (this instanceof IBattlePower bp) {
+			bp.setBattlePower(150000000);
+		}
+	}
 
-    @Override
-    public void tick() {
-        super.tick();
+	@Override
+	public void tick() {
+		super.tick();
 
-        LivingEntity target = this.getTarget();
+		LivingEntity target = this.getTarget();
 
-        handleCommonCombatMovement(target, this.isCasting(), true);
+		handleCommonCombatMovement(target, this.isCasting(), true);
 
-        if (!this.level().isClientSide) {
-            // Cooldowns
-            if (this.laserCooldown > 0) this.laserCooldown--;
-            if (this.drainCooldown > 0) this.drainCooldown--;
+		if (!this.level().isClientSide) {
+			// Cooldowns
+			if (this.laserCooldown > 0) this.laserCooldown--;
+			if (this.drainCooldown > 0) this.drainCooldown--;
 
-            if (target != null && target.isAlive() && !this.isCasting()) {
-                double distSqr = this.distanceToSqr(target);
+			if (target != null && target.isAlive() && !this.isCasting()) {
+				double distSqr = this.distanceToSqr(target);
 
-                if (this.teleportCooldown <= 0 && distSqr > 200.0D) {
-                    performTeleport(target);
-                    return;
-                }
+				if (this.teleportCooldown <= 0 && distSqr > 200.0D) {
+					performTeleport(target);
+					return;
+				}
 
-                if (this.drainCooldown <= 0 && distSqr < 9.0D) {
-                    startCasting(SKILL_ENERGY_DRAIN);
-                }
-                else if (this.laserCooldown <= 0 && distSqr > 100.0D) {
-                    startCasting(SKILL_EYE_LASER);
-                }
-            }
+				if (this.drainCooldown <= 0 && distSqr < 9.0D) {
+					startCasting(SKILL_ENERGY_DRAIN);
+				} else if (this.laserCooldown <= 0 && distSqr > 100.0D) {
+					startCasting(SKILL_EYE_LASER);
+				}
+			}
 
-            if (this.isCasting()) {
-                this.setDeltaMovement(0, 0, 0);
-                this.getNavigation().stop();
+			if (this.isCasting()) {
+				this.setDeltaMovement(0, 0, 0);
+				this.getNavigation().stop();
 
-                if (target != null && target.isAlive()) {
-                    this.castTimer++;
-                    int currentSkill = getSkillType();
+				if (target != null && target.isAlive()) {
+					this.castTimer++;
+					int currentSkill = getSkillType();
 
-                    if (currentSkill == SKILL_EYE_LASER) {
-                        this.lookAt(target, 30.0F, 30.0F);
+					if (currentSkill == SKILL_EYE_LASER) {
+						this.lookAt(target, 30.0F, 30.0F);
 
-                        if (this.castTimer == 20) {
-                            performDoubleEyeLaser(target);
-                            stopCasting();
-                        }
-                    }
+						if (this.castTimer == 20) {
+							performDoubleEyeLaser(target);
+							stopCasting();
+						}
+					} else if (currentSkill == SKILL_ENERGY_DRAIN) {
+						performGrabLogic(target);
+					}
 
-                    else if (currentSkill == SKILL_ENERGY_DRAIN) {
-                        performGrabLogic(target);
-                    }
+				} else {
+					stopCasting();
+				}
+			}
+		}
+	}
 
-                } else {
-                    stopCasting();
-                }
-            }
-        }
-    }
+	private void performGrabLogic(LivingEntity target) {
+		Vec3 viewVector = this.getViewVector(1.0F);
+		double grabDist = 1.2D;
+		double targetX = this.getX() + viewVector.x * grabDist;
+		double targetY = this.getY() + 0.5D;
+		double targetZ = this.getZ() + viewVector.z * grabDist;
 
-    private void performGrabLogic(LivingEntity target) {
-        Vec3 viewVector = this.getViewVector(1.0F);
-        double grabDist = 1.2D;
-        double targetX = this.getX() + viewVector.x * grabDist;
-        double targetY = this.getY() + 0.5D;
-        double targetZ = this.getZ() + viewVector.z * grabDist;
+		target.setPos(targetX, targetY, targetZ);
+		target.setDeltaMovement(0, 0, 0);
+		target.hurtMarked = true;
 
-        target.setPos(targetX, targetY, targetZ);
-        target.setDeltaMovement(0,0,0);
-        target.hurtMarked = true;
+		this.lookAt(target, 360, 360);
 
-        this.lookAt(target, 360, 360);
+		if (this.castTimer % 10 == 0) {
+			float damageAmount = getKiBlastDamage() / 3;
 
-        if (this.castTimer % 10 == 0) {
-            float damageAmount = getKiBlastDamage();
+			target.hurt(this.damageSources().mobAttack(this), damageAmount);
 
-            target.hurt(this.damageSources().mobAttack(this), damageAmount);
+			this.heal(damageAmount);
 
-            this.heal(damageAmount);
+			if (target instanceof Player player) {
+				StatsProvider.get(StatsCapability.INSTANCE, player).ifPresent(stats -> {
 
-            if (target instanceof Player player) {
-                StatsProvider.get(StatsCapability.INSTANCE, player).ifPresent(stats -> {
+					int maxEnergy = stats.getMaxEnergy();
+					int drainPercentage = (int) (maxEnergy * 0.05);
 
-                    int maxEnergy = stats.getMaxEnergy();
-                    int drainPercentage = (int) (maxEnergy * 0.05);
+					if (drainPercentage < 1) drainPercentage = 1;
 
-                    if (drainPercentage < 1) drainPercentage = 1;
+					stats.getResources().removeEnergy(drainPercentage);
+				});
+			}
+		}
+		if (this.castTimer >= 60) {
+			target.setDeltaMovement(viewVector.scale(1.5));
+			stopCasting();
+		}
+	}
 
-                    stats.getResources().removeEnergy(drainPercentage);
-                });
-            }
-        }
-        if (this.castTimer >= 60) {
-            target.setDeltaMovement(viewVector.scale(1.5));
-            stopCasting();
-        }
-    }
+	private void performDoubleEyeLaser(LivingEntity target) {
+		shootGenericKiLaser(target, 2.0F, 0xFFFF00, 0xFFD700);
+		shootGenericKiLaser(target, 2.0F, 0xFFFF00, 0xFFD700);
+	}
 
-    private void performDoubleEyeLaser(LivingEntity target) {
-        shootGenericKiLaser(target, 2.0F, 0xFFFF00, 0xFFD700);
-        shootGenericKiLaser(target, 2.0F, 0xFFFF00, 0xFFD700);
-    }
+	@Override
+	public void stopCasting() {
+		int usedSkill = getSkillType();
 
-    @Override
-    public void stopCasting() {
-        int usedSkill = getSkillType();
+		if (usedSkill == SKILL_EYE_LASER) {
+			this.laserCooldown = 8 * 20;
+		} else if (usedSkill == SKILL_ENERGY_DRAIN) {
+			this.drainCooldown = 15 * 20;
+		}
 
-        if (usedSkill == SKILL_EYE_LASER) {
-            this.laserCooldown = 8 * 20;
-        } else if (usedSkill == SKILL_ENERGY_DRAIN) {
-            this.drainCooldown = 15 * 20;
-        }
+		super.stopCasting();
+	}
 
-        super.stopCasting();
-    }
+	@Override
+	public boolean hurt(DamageSource pSource, float pAmount) {
+		if (this.isCasting() && getSkillType() == SKILL_ENERGY_DRAIN) {
+			return false;
+		}
+		return super.hurt(pSource, pAmount);
+	}
 
-    @Override
-    public boolean hurt(DamageSource pSource, float pAmount) {
-        if (this.isCasting() && getSkillType() == SKILL_ENERGY_DRAIN) {
-            return false;
-        }
-        return super.hurt(pSource, pAmount);
-    }
+	@Override
+	public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+		super.registerControllers(controllers);
+		controllers.add(new AnimationController<>(this, "skill_controller", 0, this::skillPredicate));
+	}
 
-    @Override
-    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
-        super.registerControllers(controllers);
-        controllers.add(new AnimationController<>(this, "skill_controller", 0, this::skillPredicate));
-    }
+	private <T extends GeoAnimatable> PlayState skillPredicate(AnimationState<T> event) {
+		if (this.isCasting()) {
+			int currentSkill = getSkillType();
 
-    private <T extends GeoAnimatable> PlayState skillPredicate(AnimationState<T> event) {
-        if (this.isCasting()) {
-            int currentSkill = getSkillType();
-
-            if (currentSkill == SKILL_EYE_LASER) {
-                return event.setAndContinue(RawAnimation.begin().thenPlay("kilaser"));
-            }
-            else if (currentSkill == SKILL_ENERGY_DRAIN) {
-                return event.setAndContinue(RawAnimation.begin().thenLoop("grab"));
-            }
-        }
-        event.getController().forceAnimationReset();
-        return PlayState.STOP;
-    }
+			if (currentSkill == SKILL_EYE_LASER) {
+				return event.setAndContinue(RawAnimation.begin().thenPlay("kilaser"));
+			} else if (currentSkill == SKILL_ENERGY_DRAIN) {
+				return event.setAndContinue(RawAnimation.begin().thenLoop("grab"));
+			}
+		}
+		event.getController().forceAnimationReset();
+		return PlayState.STOP;
+	}
 }
