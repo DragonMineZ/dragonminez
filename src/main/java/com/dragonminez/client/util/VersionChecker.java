5538d8ff503aa28a503b863434029e1c2c889c27
Bruno <brunitoaas@gmail.com>
2026-03-20 15:55:39 -0400

    initial push VersionChecker

---
+++ src/main/java/com/dragonminez/client/util/VersionChecker.java
@@ -0,0 +1,12 @@
+package com.dragonminez.client.util;
+
+import com.dragonminez.DragonMineZ;
+import com.dragonminez.Reference;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Reference.MOD_ID, value = Dist.CLIENT)
+public class VersionChecker {
+
+}
