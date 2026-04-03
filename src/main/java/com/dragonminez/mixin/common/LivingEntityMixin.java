0e6ba0f11198188aae4f3fbd54e3c37789fce057
Shokkoh <ezshokkoh@gmail.com>
2026-04-03 19:50:23 -0300

    feat: mixin to fix health value on multiplayer

--- src/main/java/com/dragonminez/mixin/common/LivingEntityMixin.java
+++ src/main/java/com/dragonminez/mixin/common/LivingEntityMixin.java
@@ -1,20 +1,56 @@
 package com.dragonminez.mixin.common;
 
 import com.dragonminez.common.init.entities.IBattlePower;
+import com.dragonminez.common.util.IHealthFixable;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin implements IBattlePower {
+public abstract class LivingEntityMixin implements IBattlePower, IHealthFixable {
 	@Shadow public abstract float getMaxHealth();
 	@Shadow public abstract AttributeMap getAttributes();
+	@Shadow public abstract float getHealth();
+	@Shadow public abstract void setHealth(float newHealth);
 
 	@Unique
 	public int battlePower = 0;
+
+	@Unique
+	private Float dragonminez$actualHealth = null;
+
+	@Inject(method = "readAdditionalSaveData(Lnet/minecraft/nbt/CompoundTag;)V", at = @At("HEAD"))
+	private void dragonminez$readAdditionalSaveData(CompoundTag tag, CallbackInfo callback) {
+		if (tag.contains("Health", Tag.TAG_ANY_NUMERIC)) {
+			float savedHealth = tag.getFloat("Health");
+			if (savedHealth > this.getMaxHealth() && savedHealth > 0.0F) {
+				this.dragonminez$actualHealth = savedHealth;
+			}
+		}
+	}
+
+	@Inject(method = "tick()V", at = @At("TAIL"))
+	private void dragonminez$restoreStoredHealth(CallbackInfo callback) {
+		if (this.dragonminez$actualHealth != null) {
+			if (this.dragonminez$actualHealth > 0.0F && this.dragonminez$actualHealth > this.getHealth()) {
+				this.setHealth(this.dragonminez$actualHealth);
+			}
+			this.dragonminez$actualHealth = null;
+		}
+	}
+
+	@Override
+	public void dragonminez$setHealthRestorePoint(float restorePoint) {
+		this.dragonminez$actualHealth = restorePoint;
+	}
 
 	@Override
 	public int getBattlePower() {
