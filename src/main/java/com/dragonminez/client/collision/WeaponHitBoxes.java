bd18e52b2f16450a52a5969bf8a3e690c8253837
Shokkoh <ezshokkoh@gmail.com>
2026-05-25 20:04:43 -0300

    feat: increased hitbox size on Z dimension

--- src/main/java/com/dragonminez/client/collision/WeaponHitBoxes.java
+++ src/main/java/com/dragonminez/client/collision/WeaponHitBoxes.java
@@ -7,14 +7,14 @@ public class WeaponHitBoxes {
     public static Vec3 createHitbox(WeaponAttributes.HitBoxShape direction, double attackRange, boolean isSpinAttack) {
         switch (direction) {
             case FORWARD_BOX -> {
-                return new Vec3(attackRange * 0.5, attackRange * 0.5, attackRange);
+                return new Vec3(attackRange * 0.5, attackRange * 0.5, 1 + attackRange);
             }
             case VERTICAL_PLANE -> {
-                float zMultiplier = isSpinAttack ? 2 : 1;
+                float zMultiplier = (float) (isSpinAttack ? 2.25 : 1.25);
                 return new Vec3(attackRange / 3.0, attackRange * 2.0, attackRange * zMultiplier);
             }
             case HORIZONTAL_PLANE -> {
-                float zMultiplier = isSpinAttack ? 2 : 1;
+                float zMultiplier = (float) (isSpinAttack ? 2.25 : 1.25);
                 return new Vec3(attackRange * 2.0, attackRange / 3.0, attackRange * zMultiplier);
             }
         }
