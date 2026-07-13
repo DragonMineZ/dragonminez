096783700cb82bf649eafa0f7541a4af9ab4f181
Shokkoh <ezshokkoh@gmail.com>
2026-07-12 21:07:54 -0300

    feat: Pair ID on item obtention + tooltips

--- src/main/java/com/dragonminez/common/init/item/DMZCuriosItem.java
+++ src/main/java/com/dragonminez/common/init/item/DMZCuriosItem.java
@@ -1,7 +1,14 @@
 package com.dragonminez.common.init.item;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
+
+import java.util.List;
 
 public class DMZCuriosItem extends Item implements ICurioItem {
 
@@ -19,5 +26,11 @@ public class DMZCuriosItem extends Item implements ICurioItem {
 
 	public CurioType getCurioType() {
 		return curioType;
+	}
+
+	@Override
+	public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
+		PothalaPairItem.appendPairIdTooltip(stack, tooltip);
+		super.appendHoverText(stack, level, tooltip, flag);
 	}
 }
--- src/main/java/com/dragonminez/common/init/item/PothalaPairItem.java
+++ src/main/java/com/dragonminez/common/init/item/PothalaPairItem.java
@@ -1,13 +1,20 @@
 package com.dragonminez.common.init.item;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class PothalaPairItem extends Item {
@@ -24,11 +31,15 @@ public class PothalaPairItem extends Item {
 	}
 
 	@Override
+	public void inventoryTick(ItemStack stack, Level level, Entity entity, int slotId, boolean isSelected) {
+		if (!level.isClientSide) ensurePairId(stack, level.random);
+	}
+
+	@Override
 	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
 		ItemStack pair = player.getItemInHand(hand);
 		if (!level.isClientSide) {
-			int pairId = level.random.nextInt();
-			if (pairId == 0) pairId = 1;
+			int pairId = ensurePairId(pair, level.random);
 
 			ItemStack left = new ItemStack(leftEarring.get());
 			ItemStack right = new ItemStack(rightEarring.get());
@@ -42,8 +53,24 @@ public class PothalaPairItem extends Item {
 		return InteractionResultHolder.sidedSuccess(pair, level.isClientSide());
 	}
 
+	@Override
+	public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
+		appendPairIdTooltip(stack, tooltip);
+		super.appendHoverText(stack, level, tooltip, flag);
+	}
+
 	private static void giveOrDrop(Player player, ItemStack stack) {
 		if (!player.addItem(stack)) player.drop(stack, false);
+	}
+
+	public static int ensurePairId(ItemStack stack, RandomSource random) {
+		int pairId = getPairId(stack);
+		if (pairId == 0) {
+			pairId = random.nextInt();
+			if (pairId == 0) pairId = 1;
+			setPairId(stack, pairId);
+		}
+		return pairId;
 	}
 
 	public static void setPairId(ItemStack stack, int id) {
@@ -53,5 +80,13 @@ public class PothalaPairItem extends Item {
 	public static int getPairId(ItemStack stack) {
 		CompoundTag tag = stack.getTag();
 		return tag != null ? tag.getInt(PAIR_ID_KEY) : 0;
+	}
+
+	public static void appendPairIdTooltip(ItemStack stack, List<Component> tooltip) {
+		int pairId = getPairId(stack);
+		if (pairId != 0) {
+			tooltip.add(Component.translatable("item.dragonminez.pothala.pair_id",
+					String.format("%08X", pairId)).withStyle(ChatFormatting.GRAY));
+		}
 	}
 }
